4a7902f8c263c3753ef389d44fd13c526c448254
Fatih Çakıroğlu <Fatih.Cakiroglu.dn@aygaz.com.tr>
2023-09-13 21:58:21 +0300

    Add project files.

---
+++ UdemyObservability.ConsoleApp/OpenTelemetryConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyObservability.ConsoleApp
+{
+    internal class OpenTelemetryConstants
+    {
+
+     internal   const string ServiceName = "ConsoleAPp"; // CompanyName.AppName.ComponentName
+     internal   const string ServiceVersion = "1.0.0";
+    }
+}
---
+++ UdemyObservability.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using UdemyObservability.ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+
+var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .ConfigureResource(configure =>
+    {
+        configure
+        .AddService(OpenTelemetryConstants.ServiceName, OpenTelemetryConstants.ServiceVersion)
+        .AddAttributes(new List<KeyValuePair<string, object>>()
+                {
+            
+                    new KeyValuePair<string, object>("host.machineName", Environment.MachineName),
+                    new KeyValuePair<string, object>("host.environment", "dev"),
+
+                });
+    }).Build();
+
+
