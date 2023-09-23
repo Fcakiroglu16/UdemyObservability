713686669088d712aab02fd92af054f9c8bfbcb2
Fatih Çakıroğlu <Fatih.Cakiroglu.dn@aygaz.com.tr>
2023-09-23 04:46:29 +0300

    bug fixed

---
+++ Logging.Shared/Logging.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Elasticsearch;
+
+namespace Logging.Shared
+{
+    public static class Logging
+    {
+       
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogging => (builderContext, loggerConfiguration) =>
+        {
+            var environment = builderContext.HostingEnvironment;
+
+
+            loggerConfiguration
+            .ReadFrom.Configuration(builderContext.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("Env", environment.EnvironmentName)
+            .Enrich.WithProperty("AppName", environment.ApplicationName);
+
+
+            var elasticsearchBaseUrl = builderContext.Configuration.GetSection("Elasticsearch")["BaseUrl"];
+            var userName = builderContext.Configuration.GetSection("Elasticsearch")["UserName"];
+            var password = builderContext.Configuration.GetSection("Elasticsearch")["Password"];
+            var indexName = builderContext.Configuration.GetSection("Elasticsearch")["IndexName"];
+
+            loggerConfiguration.WriteTo.Elasticsearch(new(new Uri(elasticsearchBaseUrl))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = Serilog.Sinks.Elasticsearch.AutoRegisterTemplateVersion.ESv8,
+                IndexFormat = $"{indexName}-{environment.EnvironmentName}-logs-"+ "{0:yyy.MM.dd}",
+                ModifyConnectionSettings = x => x.BasicAuthentication(userName, password),
+                CustomFormatter = new ElasticsearchJsonFormatter()
+            });
+
+
+        };
+   
+    }
+}
---
+++ Logging.Shared/OpenTelemetryTraceIdMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging.Shared
+{
+    public class OpenTelemetryTraceIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public OpenTelemetryTraceIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            var logger = context.RequestServices.GetRequiredService<ILogger<OpenTelemetryTraceIdMiddleware>>();
+
+            var traceId = Activity.Current?.TraceId.ToString();
+
+            using (logger.BeginScope("{@traceId}", traceId))
+            {
+                await _next(context);
+            }
+
+
+
+
+        }
+    }
+}
